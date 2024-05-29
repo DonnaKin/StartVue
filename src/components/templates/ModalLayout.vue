118784e853f319cb872e5fed572cf1b620c8f5f5
BOOK-B75PD5MQMV\선이권 <sun@tobecon.net>
2024-05-29 15:57:18 +0900

    모달 모듈 추가

--- src/components/templates/ModalLayout.vue
+++ src/components/templates/ModalLayout.vue
@@ -2,17 +2,21 @@
     <teleport to="#modalLayout">
         <div class="dim"></div>
 
-        <div class="popup_wrap">
+        <div class="popup_wrap" :style="mstyle">
             <div class="popup type_01">
                 <strong class="terms_title">{{ title }}</strong>
 
-                <div class="terms_text_wrap">
-                    <div class="text_box">
-                        <component :is="contentComp" />
+                <div class="Modal_content_wrap">
+                    <LoadingBar v-if="isLoading"/>
+
+                    <div class="text_box" v-else>
+                        <slot name="content"></slot>
                     </div>
                 </div>
 
-                <button type="button" class="btn" @click="CloseModal">동의</button>
+                <div>
+                    <slot name="footer"></slot>
+                </div>
             </div>
         </div>
     </teleport>
@@ -22,14 +26,12 @@
 /***********************************************************************************************************/
 /*                                             I M P O R T                                                 */
 /***********************************************************************************************************/
-    import { toRefs, ref, onMounted, reactive, computed, Component } from 'vue'
+    import { defineProps, toRefs } from 'vue'
+    import LoadingBar from '../atoms/etc/LoadingBar.vue';
 
 /***********************************************************************************************************/
 /*                                               D A T A                                                   */
 /***********************************************************************************************************/
-    const title = ref<string>('');
-    const content = reactive<any>(Object.create(null));
-    const init_ModalItems = ref<Array<ModalItem>>([]);
 
 
 /***********************************************************************************************************/
@@ -40,12 +42,14 @@
 /***********************************************************************************************************/
 /*                                              P R O P S                                                  */
 /***********************************************************************************************************/
+    const props = withDefaults(defineProps<{ title: string, mstyle: string, isLoading: boolean }>(), { title: '모달 제목을 넣으세요.', mstyle: 'width: 800px; height: 400px;', isLoading: true });
+    const { title, mstyle, isLoading } = toRefs(props);
+
 
 /***********************************************************************************************************/
 /*                                           L I F E C Y C L E                                             */
 /***********************************************************************************************************/
-    onMounted(() => {
-    })
+
 
 /***********************************************************************************************************/
 /*                                             M E T H O D                                                 */
@@ -55,13 +59,19 @@
 /***********************************************************************************************************/
 /*                                               E M I T                                                   */
 /***********************************************************************************************************/
-    const emit = defineEmits(['CloseModal'])
-
-    const CloseModal = () => {
-        if (init_ModalItems.value.length == 1) {
-            emit('CloseModal')
-            return
-        }
-    }
     
 </script>
+
+<style scoped>
+    .dim { display: block; width: 100%; height: 100%; position: fixed; left: 50%; top: 0;  background-color: rgba(0,0,0,0.7); transform: translateX(-50%); z-index: 100; } 
+    
+    .popup_wrap { display: block; width: 100%; height: 100%; position: fixed; left: 50%; max-width: 786px; min-width: 360px; transform: translateX(-50%); z-index: 200; } 
+    .popup_wrap .popup { display: block; z-index: 250; } 
+    .popup_wrap .popup.active { display: block; } 
+    .popup_wrap .popup.type_01 { position: absolute; left: 50%; transform: translate(-50%, -50%); width: calc(100% - 48px); background-color: #ffffff; text-align: center; padding: 24px; border-radius: 5px; } 
+    .popup_wrap .popup.type_01 .Modal_content_wrap { width: 100%; height: 45vh; max-height: 400px; padding: 12px; background: #f4f4f4; margin-top: 20px; overflow-y: scroll; }
+    .popup_wrap .popup.type_01 .title { font-weight: 700; font-size: 16px; line-height: 24px; color: #333333; } 
+    .popup_wrap .popup.type_01 .title { font-weight: 700; font-size: 16px; line-height: 24px; color: #333333; } 
+    .popup_wrap .popup.type_01 .text { margin-top: 20px; font-weight: 300; font-size: 14px; line-height: 115%; letter-spacing: 0.05em; color: #666666; } 
+    .popup_wrap .popup.type_01 .btn { margin-top: 20px; display: block; width: 100%; text-align: center; height: 56px; line-height: 56px; background-color: #F37321; font-weight: 500; font-size: 18px; color: #ffffff; border-radius: 8px; } 
+</style>
