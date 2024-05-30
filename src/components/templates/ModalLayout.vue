123d21b44ae667ec24da183950ead197ccd7b053
BOOK-B75PD5MQMV\선이권 <sun@tobecon.net>
2024-05-30 16:47:06 +0900

    ModalLayout 수정

--- src/components/templates/ModalLayout.vue
+++ src/components/templates/ModalLayout.vue
@@ -4,7 +4,7 @@
 
         <div class="popup_wrap">
             <div class="popup type_01" :style="modalStyle">
-                <strong class="terms_title">{{ title }}</strong>
+                <strong class="title">{{ title }}</strong>
 
                 <div class="Modal_content_wrap" :style="contentStyle">
                     <LoadingBar v-if="isLoading"/>
@@ -14,9 +14,7 @@
                     </div>
                 </div>
 
-                <div>
-                    <slot name="footer"></slot>
-                </div>
+                <slot name="footer"></slot>
             </div>
         </div>
     </teleport>
@@ -42,7 +40,7 @@
     })
 
     const contentStyle = computed(() => {
-        return `height: ${height.value - 100}px`;
+        return `height: ${height.value - 200}px`;
     })
 
 /***********************************************************************************************************/
@@ -72,12 +70,10 @@
     .dim { display: block; width: 100%; height: 100%; position: fixed; left: 50%; top: 0;  background-color: rgba(0,0,0,0.7); transform: translateX(-50%); z-index: 100; } 
     
     .popup_wrap { display: block; width: 100%; height: 100%; position: fixed; left: 50%; max-width: 786px; min-width: 360px; transform: translateX(-50%); z-index: 200; } 
-    .popup_wrap .popup { display: block; z-index: 250; } 
+    .terms_title { font-size: 30px; }
+    .popup_wrap .popup { display: block; z-index: 250; }
     .popup_wrap .popup.active { display: block; } 
     .popup_wrap .popup.type_01 { position: absolute; left: 50%; transform: translate(-50%, -50%); width: calc(100% - 48px); background-color: #ffffff; text-align: center; padding: 24px; border-radius: 5px; } 
-    .popup_wrap .popup.type_01 .Modal_content_wrap { width: 100%; height: 45vh; max-height: 400px; padding: 12px; background: #f4f4f4; margin-top: 20px; overflow-y: scroll; }
-    .popup_wrap .popup.type_01 .title { font-weight: 700; font-size: 16px; line-height: 24px; color: #333333; } 
-    .popup_wrap .popup.type_01 .title { font-weight: 700; font-size: 16px; line-height: 24px; color: #333333; } 
-    .popup_wrap .popup.type_01 .text { margin-top: 20px; font-weight: 300; font-size: 14px; line-height: 115%; letter-spacing: 0.05em; color: #666666; } 
-    .popup_wrap .popup.type_01 .btn { margin-top: 20px; display: block; width: 100%; text-align: center; height: 56px; line-height: 56px; background-color: #F37321; font-weight: 500; font-size: 18px; color: #ffffff; border-radius: 8px; } 
+    .popup_wrap .popup.type_01 .Modal_content_wrap { width: 100%; height: 100%; padding: 12px; background: #f4f4f4; margin-top: 20px; overflow-y: scroll; }
+    .popup_wrap .popup.type_01 .title { font-weight: 700; font-size: 30px; line-height: 24px; color: #333333; } 
 </style>
